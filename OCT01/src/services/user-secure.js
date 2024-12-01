c5343b3b53723aec85e4d9c72cd6dd33f9c7fd6a
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-12-01 17:15:53 +0000

    Acess Refresh tokens

--- OCT01/src/services/user-secure.js
+++ OCT01/src/services/user-secure.js
@@ -1,8 +1,14 @@
 import jwt from "jsonwebtoken";
+
 class Secure {
-  generateToken(data) {
+  generateAccessToken(data) {
     return jwt.sign(data, process.env.ACCESS_KEY_JWT, { expiresIn: "15m" });
   }
+
+  generateRefreshToken(data) {
+    return jwt.sign(data, process.env.REFRESH_KEY_JWT, { expiresIn: "7d" });
+  }
+
   authenticateToken(req, res, next) {
     const auth = req.headers["authorization"];
     const token = auth && auth.split(" ")[1];
@@ -18,7 +24,21 @@ class Secure {
         }
       });
     }
-    res.send(token);
+  }
+
+
+  refreshAccessToken(req, res) {
+    const refreshToken = req.cookies["refresh_token"];
+    if (refreshToken == null) {
+      return res.sendStatus(401); 
+    }
+    jwt.verify(refreshToken, process.env.REFRESH_KEY_JWT, (err, user) => {
+      if (err) {
+        return res.sendStatus(403); 
+      }
+      const newAccessToken = this.generateAccessToken({ login: user.login, email: user.email });
+      res.json({ accessToken: newAccessToken });
+    });
   }
 }
 
---
+++ OCT14/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import https from "node:https";
+import path from "node:path";
+import fs from "node:fs";
+const PORT = 443;
+const app = express();
+const __dirname = import.meta.dirname;
+const options = {
+    key: fs.readFileSync(path.join(__dirname, "..", "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "..", "cert", "cert.pem"))
+};
+https
+    .createServer(options, app)
+    .listen(PORT, () => console.log("https://127.0.0.1/"));
+app.get("/", (req, res) => {
+    res.send("SERVER HTTPS");
+});
